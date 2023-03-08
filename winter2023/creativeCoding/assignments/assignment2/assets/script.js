d02da3d3afed7250759b682cb8b1519fd6af9771
Mafuriyu <marisellejbc@gmail.com>
2023-03-07 23:21:02 -0500

    99% Added comments

--- winter2023/creativeCoding/assignments/assignment2/assets/script.js
+++ winter2023/creativeCoding/assignments/assignment2/assets/script.js
@@ -1,5 +1,7 @@
 
 // variables are outside so all functions can access them
+// var num is for total clicks available
+// color counter and color pressed is for the color to check if they are called/clicked
 var num = 3;
 var blueCount = 0;
 var redCount = 0;
@@ -9,6 +11,7 @@ var bluePressed = Boolean(false);
 var redPressed = Boolean(false);
 var yellowPressed = Boolean(false);
 
+//onload so that it will be checked/called as soon as the page loads/refereshes
 function startUp() {
     document.getElementById("num").innerHTML = num;
 }
@@ -18,6 +21,7 @@ function startUp() {
 function buttonPressed(img){
     //checking if the image id is blue
     if(img.id == "blue"){
+        //checks if the there are still available color clicks(num)
         if((num <= 3) && (num >= 0)){
             if(bluePressed == false){
                 num -=1;
@@ -42,6 +46,7 @@ function buttonPressed(img){
     
     //checking if the image id is red
     else if(img.id == "red"){
+        //checks if the there are still available color clicks(num)
         if((num <= 3) && (num >= 0)){
             if(redPressed == false){
                 num -=1;
@@ -66,6 +71,7 @@ function buttonPressed(img){
 
     //checking if the image id is yellow
     else if(img.id == "yellow"){
+        //checks if the there are still available color clicks(num)
         if((num <= 3) && (num >= 0)){
             if(yellowPressed == false){
                 num -=1;
@@ -88,13 +94,14 @@ function buttonPressed(img){
         }
     }
 
+    //The next block of codes are for the character change. 
+    //If the conditions are met, it will change into the approriate images/characters
+
     // blue + red + yellow = brown
     if((bluePressed == true) && (redPressed == true) && (yellowPressed == true)){
         document.getElementById("title").style.color="rgb(110, 38, 14)";
         document.getElementById("character").src = "assets/images/chocoChara.png";
         document.getElementById("mixedColor").innerHTML="Choco Decora";
-        
-        
     }
     // blue + red = violet
     else if ((bluePressed == true) && redPressed == true){
@@ -143,5 +150,4 @@ function buttonPressed(img){
         document.getElementById("character").src = "assets/images/defaultChara.png";
         document.getElementById("mixedColor").innerHTML="Normal";
     }
-
 }
